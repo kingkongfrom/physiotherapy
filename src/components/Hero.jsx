b0ee667427713ae1d4c93b588d844210269cba84
kingkongfrom <eduardo@blackvoodoocr.com>
2023-11-18 12:20:44 -0600

    Login / Sign up form styled Hero section added to Homepage

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,45 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Hero = () => {
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
+
+    const buttonStyles = {
+        margin: "1rem",
+        color: isHovered ? "#fff" : "#0A6E7C",
+        fontSize: "1rem",
+        fontWeight: "700",
+        padding: "0.7rem 1.4rem",
+        backgroundColor: isHovered ? "#0A6E7C" : "#fff",
+        borderRadius: "5px",
+        transition: "background-color 0.3s ease-in-out, color 0.3s ease-in-out",
+    };
+
     return (
         <div style={styles.container}>
             <h1 style={styles.h1}>Dra. Priscilla Rodriguez</h1>
-            <h3 style={styles.h3}>Dedicated to patient care and wellness for the entire family.</h3>
-            <button style={styles.button}>Reserva tu Cita</button>
+            <h3 style={styles.h3}>
+                Dedicated to patient care and wellness for the entire family.
+            </h3>
+            <Link
+                to="/booking"
+                style={buttonStyles}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+            >
+                Reserva tu Cita
+            </Link>
         </div>
     );
-}
+};
 
 const styles = {
     container: {
@@ -31,16 +63,8 @@ const styles = {
         width: "13rem",
         textAlign: "center",
         color: "#fff",
-        fontSize: "0.9rem"
+        fontSize: "0.9rem",
     },
-    button: {
-        margin: "1rem",
-        color: "#0A6E7C",
-        fontSize: "1rem",
-        fontWeight: "700",
-        padding: "1rem 2rem",
-        backgroundColor: "#fff",
-        borderRadius: "5px",
-    },
-}
+};
+
 export default Hero;
